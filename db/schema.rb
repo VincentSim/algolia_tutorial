ad2b409e222b14bf7ab564cdcbea9ecaaf75e6ad
Vincent Simon <vincent.simon@gadz.org>
2016-02-29 09:45:19 +0100

    pg db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160208165619) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "team_members", force: :cascade do |t|
     t.string   "name"
     t.string   "function"
